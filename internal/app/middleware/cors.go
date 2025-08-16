4491a82c127b30d38f7fdfd0292d0f4f1aa9c956
Maksim Necheporuk <mnecheporuka@ozon.ru>
2025-08-16 18:16:17 +0300

    Фиксы cors для теста

--- internal/app/middleware/cors.go
+++ internal/app/middleware/cors.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"net/http"
-	"net/url"
 )
 
 type CorsConfig interface {
@@ -17,20 +16,26 @@ func CORSMiddleware(corsConfig CorsConfig) func(next http.Handler) http.Handler 
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			//origin := r.Header.Get("Origin")
+			//
+			//if origin != "" {
+			//	if u, err := url.Parse(origin); err == nil {
+			//		host := u.Host
+			//
+			//		if _, exists := allowedHosts[host]; exists {
+			//			w.Header().Set("Access-Control-Allow-Origin", origin)
+			//			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
+			//			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+			//			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			//		}
+			//	}
+			//}
 			origin := r.Header.Get("Origin")
 
-			if origin != "" {
-				if u, err := url.Parse(origin); err == nil {
-					host := u.Host
-
-					if _, exists := allowedHosts[host]; exists {
-						w.Header().Set("Access-Control-Allow-Origin", origin)
-						w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-						w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
-						w.Header().Set("Access-Control-Allow-Credentials", "true")
-					}
-				}
-			}
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 			if r.Method == "OPTIONS" {
 				w.WriteHeader(http.StatusNoContent)
--- internal/app/router/v1/router.go
+++ internal/app/router/v1/router.go
@@ -33,7 +33,7 @@ func NewRouter(
 
 func (r *Router) setupMiddlewares() {
 	r.Router.Use(base_middleware.Recoverer)
-	//r.Router.Use(middleware.CORSMiddleware(r.staticConfig)) // todo вернуть
+	r.Router.Use(middleware.CORSMiddleware(r.staticConfig))
 	//s.router.Use(middleware.CSRFMiddleware)
 	r.Router.Use(middleware.ConfigMiddleware(r.mutableConfig))
 	r.Router.Use(middleware.LoggerMiddleware(logger.New()))
