9ddefc8c6dfdfe6a292be9c3570e4d21776e866d
Maksim Necheporuk <mnecheporuka@ozon.ru>
2025-12-22 19:01:01 +0300

    Доработки ютуба

--- internal/modules/doctors/client/subscribers/indto/request.go
+++ internal/modules/doctors/client/subscribers/indto/request.go
@@ -8,6 +8,7 @@ const (
 	All SocialMedia = iota
 	Telegram
 	Instagram
+	Youtube
 )
 
 func (sm SocialMedia) String() string {
@@ -16,6 +17,8 @@ func (sm SocialMedia) String() string {
 		return "tg"
 	case Instagram:
 		return "inst"
+	case Youtube:
+		return "youtube"
 	default:
 		return ""
 	}
@@ -27,6 +30,8 @@ func NewSocialMedia(sm string) SocialMedia {
 		return Telegram
 	case "inst":
 		return Instagram
+	case "youtube":
+		return Youtube
 	}
 
 	return All
