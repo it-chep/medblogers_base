fe2cd4f2dc0469f87a35f05be2ae1e36ef1bba2e
it-chep <mj0rdan@yandex.ru>
2026-04-20 14:45:03 +0300

    Правки sql

--- migrations/20260419090000_freelancer_med_education_price_list_search.sql
+++ migrations/20260419090000_freelancer_med_education_price_list_search.sql
@@ -4,17 +4,14 @@ alter table freelancer
 
 alter table freelancers_price_list
     add column if not exists price_to integer,
-    add column if not exists search_text text,
     add column if not exists search_vector tsvector;
 
 alter table freelancers_price_list
     drop constraint if exists freelancers_price_list_freelancer_id_name_price_key;
 
 update freelancers_price_list
-set search_text = name,
-    search_vector = to_tsvector('russian', coalesce(name, ''))
-where search_text is distinct from name
-   or search_vector is null;
+set search_vector = to_tsvector('russian', coalesce(name, ''))
+where search_vector is null;
 
 create index if not exists idx_freelancers_price_list_search_vector
     on freelancers_price_list
@@ -25,7 +22,6 @@ drop index if exists idx_freelancers_price_list_search_vector;
 
 alter table freelancers_price_list
     drop column if exists search_vector,
-    drop column if exists search_text,
     drop column if exists price_to;
 
 alter table freelancers_price_list
